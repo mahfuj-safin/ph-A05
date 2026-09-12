214dbc915721b8f5042a6e9408e8ce22c4377a76
safin <mahfuzsafin@gmail.com>
2026-09-13 02:59:17 +0600

    NavBar section desing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,31 @@
+import logoText from '../assets/logo-text.png';
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center h-16 border-gray-100 border-b bg-white">
-      <div>Dev Stack</div>
-      <div>
-        <a href="">Home</a>
-        <a href="">Technologies</a>
-        <a href="">Projects</a>
-        <a href="">About</a>
-        <a href="">Contact</a>
-      </div>
-      <div>
-        <button>Sing In</button>
-        <button>Sing Up</button>
+    // NavBar
+    <nav className="border-b border-gray-200 bg-white">
+      <div className="flex justify-between items-center px-10 py-5 container mx-auto">
+        {/* logo */}
+        <div>
+          <img src={logoText} alt="" />
+        </div>
+
+        {/* Navigation Item */}
+        <div className="flex items-center gap-8">
+          <a className="text-sm text-red-500">Home</a>
+          <a className="text-sm text-gray-500">Technologies </a>
+          <a className="text-sm text-gray-500">Projects</a>
+          <a className="text-sm text-gray-500">About</a>
+          <a className="text-sm text-gray-500">Contact</a>
+        </div>
+
+        {/* sing in & sing up button*/}
+
+        <div className="flex items-center gap-4">
+          <button className="text-sm text-gray-700">Sing In</button>
+          <button className="text-sm text-white rounded-full bg-gradient-to-r from-orange-500 via-pink-500 to-purple-600 px-5 py-2">
+            Sing Up
+          </button>
+        </div>
       </div>
     </nav>
   );
